b6373ab7dae81bb93dea046bb9baaf01ae181798
whustone007 <43139865+whustone007@users.noreply.github.com>
2018-10-17 23:20:58 +0800

    Add files via upload

---
+++ homework5/program1/OrderService.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class OrderService
+    {
+        public List<int> Nums = new List<int>();
+        public List<string> goodName = new List<string>();
+        public List<string> customerName = new List<string>();
+        public List<double> prices = new List<double>();
+        
+        //增加
+        public void addOrder(int num)
+        {
+            Nums.Add(num);
+        }
+        public void addGoodName(string name)
+        {
+            goodName.Add(name);
+        }
+        public void addCustomerName(string name)
+        {
+            customerName.Add(name);
+        }
+        public void addPrice(double price)
+        {
+            prices.Add(price);
+        }
+        //删除
+        public void deleteNum(int num)
+        {
+            Nums.Remove(Nums[num]);
+        }
+        public void deleteGoodName(int num)
+        {
+            string goodname = goodName[num];
+            goodName.Remove(goodname);
+        }
+        public void deleteCustomerName(int num)
+        {
+            string customername = customerName[num];
+            customerName.Remove(customername);
+        }
+        public void deletePrice(int num)
+        {
+            double price = prices[num];
+            prices.Remove(price);
+        }
+        //修改
+        public bool changeNum(int oldNum, int newNum)
+        {
+            bool N = true;
+            for(int i=0;i < Nums.Count;i++)
+            {
+                if(Nums[i]==oldNum)
+                {
+                    Nums[i] = newNum;
+                    N = true;
+                }
+                else
+                {
+                    if(i == Nums.Count - 1)
+                    {
+                        N = false;
+                    }
+                }
+            }
+            return N;
+        }
+        public bool changeGoodName(string oldGoodName,string newGoodName)
+        {
+            bool N = true;
+            for (int i = 0; i < goodName.Count; i++)
+            {
+                if (goodName[i] == oldGoodName)
+                {
+                    goodName[i] = newGoodName;
+                    N = true;
+                }
+                else
+                {
+                    if (i == goodName.Count - 1)
+                    {
+                        N = false;
+                    }
+                }
+            }
+            return N;
+        }
+        public bool changeCustomerName(string oldCustomerName, string newCustomerName)
+        {
+            bool N = true;
+            for (int i = 0; i < customerName.Count; i++)
+            {
+                if (customerName[i] == oldCustomerName)
+                {
+                    customerName[i] = newCustomerName;
+                    N = true;
+                }
+                else
+                {
+                    if (i == customerName.Count - 1)
+                    {
+                        N = false;
+                    }
+                }
+            }
+            return N;
+        }
+
+        //查询目标的下标
+        public int searchByNum(int Num)
+        {
+            int flag = 0;
+            for(int i=0;i<Nums.Count;i++)
+            {
+                if (Nums[i] == Num)
+                {
+                    flag = i;
+                    break;
+                }
+                else
+                {
+                    if (i == Nums.Count - 1)
+                    {
+                        Exception e0 = new Exception();
+                        throw e0;
+                    }
+                }
+                    
+            }
+            return flag;
+        }
+        public int searchByGoodName(string gN)
+        {
+            int flag = 0;
+            for (int i = 0; i < goodName.Count; i++)
+            {
+                if (goodName[i] == gN)
+                {
+                    flag = i;
+                    break;
+                }
+                else
+                {
+                    if (i == Nums.Count - 1)
+                    {
+                        Exception e1 = new Exception();
+                        throw e1;
+                    }
+                }
+            }
+            return flag;
+        }
+        public int searchByCustomerName(string cN)
+        {
+            int flag = 0;
+            for (int i = 0; i < customerName.Count; i++)
+            {
+                if (customerName[i] == cN)
+                {
+                    flag = i;
+                    break;
+                }
+                else
+                {
+                    if (i == Nums.Count - 1)
+                    {
+                        Exception e2 = new Exception();
+                        throw e2;
+                    }
+                }
+            }
+            return flag;
+        }
+        public int searchByPrice(double Pri)
+        {
+            int flag = 0;
+            for (int i = 0; i < prices.Count; i++)
+            {
+                if (prices[i] == Pri)
+                {
+                    flag = i;
+                    break;
+                }
+                else
+                {
+                    if (i == prices.Count - 1)
+                    {
+                        Exception e0 = new Exception();
+                        throw e0;
+                    }
+                }
+
+            }
+            return flag;
+        }
+    }
+}
---
+++ homework5/program2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            //将输入的值赋给变量参数
+            double th1, th2, per1, per2, k, color;
+            th1 = double.Parse(textBox1.Text);
+            th2 = double.Parse(textBox2.Text);
+            per1 = double.Parse(textBox3.Text);
+            per2 = double.Parse(textBox4.Text);
+            k = double.Parse(textBox5.Text);
+            string colorString = comboBox1.Text;
+            if (colorString == "红色")
+            { color = 1; }
+            else if (colorString == "绿色")
+            { color = 2; }
+            else if (colorString == "紫色")
+            { color = 3; }
+            else if (colorString == "黑色") 
+            { color = 4; }
+            else 
+            { color = 5; }
+            double th0 = -Math.PI / 2;
+
+            drawCayleyTree(200 ,310,10, th1, th2,100 , th0, per1, per2, k, color);
+
+            
+        }
+        private void drawCayleyTree(double x0,double y0,int n,double th1,double th2,double leng,double th,
+            double per1,double per2,double k,double color)
+        {
+            if(n == 0){return;}
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1,color);
+
+            drawCayleyTree(x1, y1, n - 1, th1, th2, per1 * leng, th + th1, per1, per2, k, color);
+            drawCayleyTree(x1, y1, n - 1, th1, th2, per2 * leng, th - th2, per1, per2, k, color);
+        }
+        void drawLine(double x0,double y0,double x1,double y1,double color)
+        {
+            if(color == 1) { graphics.DrawLine(Pens.Red, (int)x0, (int)y0, (int)x1, (int)y1);}
+            if (color == 2) { graphics.DrawLine(Pens.Green, (int)x0, (int)y0, (int)x1, (int)y1); }
+            if (color == 3) { graphics.DrawLine(Pens.Purple, (int)x0, (int)y0, (int)x1, (int)y1); }
+            if (color == 4) { graphics.DrawLine(Pens.Black, (int)x0, (int)y0, (int)x1, (int)y1); }
+            if (color == 5) { graphics.DrawLine(Pens.Pink, (int)x0, (int)y0, (int)x1, (int)y1); }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            graphics.Clear(BackColor);
+        }
+    }
+}

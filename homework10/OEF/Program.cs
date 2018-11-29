3025fda8770d3766cf1575e717e7aef1562ecfd3
whustone007 <43139865+whustone007@users.noreply.github.com>
2018-11-30 00:08:15 +0800

    Add files via upload

---
+++ homework10/OEF/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OEF
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome!");
+            OrderService orderService = new OrderService();
+            List<OrderDetail> details = new List<OrderDetail>() {
+                new OrderDetail("1", "computer", 10.0, 20),
+                new OrderDetail("2", "iPad", 2.0, 100)
+            };
+            Order order = new Order("001", "slh", DateTime.Now, details);
+
+            orderService.Add(order);
+
+            Order order2 = new Order("002", "slh", DateTime.Now, details);
+            orderService.Update(order2);
+
+
+            List<Order> orders = orderService.QueryByCustormer("slh");
+            orders.ForEach(
+              o => Console.WriteLine($"{o.Id},{o.Customer}"));
+
+        }
+    }
+}
